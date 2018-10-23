810fc1a6d92331c48ddaf59e39c1aa2d9a0bb541
Chido Nguyen <chidonguyen@hotmail.com>
2018-10-23 11:11:26 -0700

    cardtest1

--- projects/nguychid/dominion/cardtest1.c
+++ projects/nguychid/dominion/cardtest1.c
@@ -27,7 +27,7 @@ void testSmith() {
 	printf("TESTING SMITHY +3 CARDS(NET GAIN OF 2 EXPECTED):\n");
 	int pre_draw = state->handCount[0];
 	printf("CURRENT HAND COUNT:\t\t %i\n", pre_draw);
-	int result = cardEffect(smithy, choice1, choice2, choice3, state, handPos, &bonus);
+	//int result = cardEffect(smithy, choice1, choice2, choice3, state, handPos, &bonus);
 	printf("EXPECTED HAND COUNT AFTER DRAW:\t\t%i\n", pre_draw + 2);
 	printf("AFTER DRAWING HAND COUNT: \t\t%i\n", state->handCount[0]);
 	if (state->handCount[0] - 2 == pre_draw)
