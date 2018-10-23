303ce0670e8447181cc56320dbb52b70d4b8986f
Chido Nguyen <chidonguyen@hotmail.com>
2018-10-23 13:03:07 -0700

    cardtest2

--- projects/nguychid/dominion/cardtest2.c
+++ projects/nguychid/dominion/cardtest2.c
@@ -11,6 +11,7 @@
 // discard the others
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus);
 void testAdvent() {
+	int x = 0, y = 0;
 	int numPlayers = 2;
 	int choice1 = 0, choice2 = 0, choice3 = 0, handPos = 0, bonus = 0;
 	int k[10] = { adventurer, embargo, village, outpost, mine, cutpurse,
@@ -29,12 +30,22 @@ void testAdvent() {
 	printf("TESTING ADVENTURER CARD:\n");
 	int results = cardEffect(adventurer, choice1, choice2, choice3, state, handPos, &bonus);
 	printf("Discarding Adventurer and Drawing Until 2 Treasure Cards: Net Gain Expected of +1 Cards\n");
-	int handTreasure = originalState->handCount[0];
-	int	postHandTreasure = state->handCount[0];
-	for (int x = 0; x < (sizeof(state->hand[0])/sizeof(state->hand[0][0])); x++) {
-		printf("%i\n", state->hand[0][x]);
+	int handTreasure = 0 ;
+	int	postHandTreasure = 0;
+	
+	while (state->hand[0][x] != 0) {
+		int tmp = state->hand[0][x];
+		if (tmp >= 4 && tmp <= 6)
+			postHandTreasure++;
+		x++
 	}
-
+	while (originalState->hand[0][x] != 0) {
+		int tmp = state->hand[0][x];
+		if (tmp >= 4 && tmp <= 6)
+			handTreasure++;
+		y++
+	}
+	printf("Orignal State: \t%i\tPost Play State: \t%i\n", handTreasure, postHandTreasure);
 	if ( postHandTreasure - 1 == handTreasure)
 		printf("PASS\n");
 	else
