b375d50f0c0f26b0ac33a98fe7042f7995488655
Chido Nguyen <chidonguyen@hotmail.com>
2018-11-12 14:11:52 -0800

    assignment 4

--- projects/nguychid/dominion/randomtestadventurer.c
+++ projects/nguychid/dominion/randomtestadventurer.c
@@ -28,32 +28,33 @@ int main(int argc, char* argv[]) {
 	int choice2 = 0;
 	int choice3 = 0;
 	int bonus = 0;
+	int handPos = 0;
 	int k[10] = { adventurer, embargo, village, outpost, mine, cutpurse, sea_hag, great_hall, smithy, council_room };
 	
 	for (int x = 0; x < TESTRUN; x++) {
 		int randomSeed = rand();
 		// initialize new game and copy original game state
-		initializeGame(numberPlayers, k, randomSeed,game);
-		memcpy(&originalGame, game, sizeof(struct gameState));
+		initializeGame(numberPlayers, k, randomSeed, &game);
+		memcpy(&originalGame, &game, sizeof(struct gameState));
 		
 		// Play the card 
 
-		cardEffect(adventurer, choice1, choice2, choice3, state, handPos, &bonus);
+		cardEffect(adventurer, choice1, choice2, choice3, game, handPos, &bonus);
 
 		// iterate through the hand make sure we've gained 2 treasure cards
 		int handTreasure = 0;
 		int	postHandTreasure = 0;
 		int y = 0, z = 0;
 		// count hand of game playing card
-		while (state->hand[0][z] != 0) {
-			int tmp = state->hand[0][z];
+		while (game->hand[0][z] != 0) {
+			int tmp = game->hand[0][z];
 			if (tmp >= 4 && tmp <= 6)
 				postHandTreasure++;
 			z++;
 		}
 		// count hand of copy of game pre-card play
-		while (originalState->hand[0][y] != 0) {
-			int tmp = state->hand[0][y];
+		while (originalGame->hand[0][y] != 0) {
+			int tmp = originalGame->hand[0][y];
 			if (tmp >= 4 && tmp <= 6)
 				handTreasure++;
 			y++;
