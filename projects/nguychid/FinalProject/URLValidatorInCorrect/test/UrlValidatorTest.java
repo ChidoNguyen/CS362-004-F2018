0b78d644583fda956566108487a568a87936ca67
Chido Nguyen <chidonguyen@hotmail.com>
2018-11-27 17:39:44 -0800

    partB

--- projects/nguychid/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/nguychid/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -26,15 +26,41 @@ public class UrlValidatorTest extends TestCase {
    }
    
    
-   public void testYourFirstPartition()
+   public void testScheme()
    {
-	 //You can use this function to implement your First Partition testing	   
-
+	 //Scheme Testing	
+	   UrlValidator urlVal = new UrlValidator(null,null,UrlValidator.ALLOW_ALL_SCHEMES);
+	    
+	   assertTrue(urlVal.isValid(testUrlScheme[0].item+"www.google.com"));
+	   assertTrue(urlVal.isValid(testUrlScheme[1].item+"www.google.com"));
+	   assertTrue(urlVal.isValid(testUrlScheme[4].item+"www.google.com"));
+	   /*assertFalse(urlVal.isValid(testUrlScheme[2].item+"www.google.com"));
+	   assertFalse(urlVal.isValid(testUrlScheme[3].item+"www.google.com"));
+	   assertFalse(urlVal.isValid(testUrlScheme[5].item+"www.google.com"));
+	   assertFalse(urlVal.isValid(testUrlScheme[6].item+"www.google.com"));
+	   assertFalse(urlVal.isValid(testUrlScheme[7].item+"www.google.com"));*/
+	   
+	   
    }
    
-   public void testYourSecondPartition(){
+   public void testAuth(){
 		 //You can use this function to implement your Second Partition testing	   
-
+	   UrlValidator urlVal = new UrlValidator(null,null,UrlValidator.ALLOW_ALL_SCHEMES);
+	    
+	   assertTrue(urlVal.isValid("http://"+testUrlAuthority[0].item));
+	   assertTrue(urlVal.isValid("http://"+testUrlAuthority[1].item));
+	   assertTrue(urlVal.isValid("http://"+testUrlAuthority[2].item));
+	   //This is wrong//
+	   assertTrue(urlVal.isValid("http://"+testUrlAuthority[3].item));
+	   assertTrue(urlVal.isValid("http://"+testUrlAuthority[4].item));
+	   assertTrue(urlVal.isValid("http://"+testUrlAuthority[5].item));
+	   assertTrue(urlVal.isValid("http://"+testUrlAuthority[6].item));
+	   //this is right//
+	  //assertFalse(urlVal.isValid("http://"+testUrlAuthority[3].item));
+	 assertFalse(urlVal.isValid("http://"+testUrlAuthority[4]));
+	 //  assertFalse(urlVal.isValid("http://"+testUrlAuthority[5]));
+	  // assertFalse(urlVal.isValid("http://"+testUrlAuthority[6]));
+	   
    }
    //You need to create more test cases for your Partitions if you need to 
    
@@ -44,6 +70,25 @@ public class UrlValidatorTest extends TestCase {
 
    }
    
-
-
+   // Taken from part A //
+   ResultPair[] testUrlScheme = {
+		   new ResultPair("http://", true),
+		   new ResultPair("https://", true),
+		   new ResultPair("h77p://", false),
+		   new ResultPair("h77ps://", false),
+		   new ResultPair("ftp://", true),
+		   new ResultPair("http:/", false),
+		   new ResultPair("http/", false),
+		   new ResultPair("http:", false),
+		   
+   };
+   ResultPair[] testUrlAuthority = {
+           new ResultPair("254.254.254.250", true),
+           new ResultPair("www.multi-word-check.com", true),
+           new ResultPair("www.gmail.com", true),
+           new ResultPair("www.------------.com", false),
+           new ResultPair("www-wrong-multi-format", false),
+           new ResultPair("256.256.256.256", false),
+           new ResultPair("0.0.0.0.0", false)};
+          
 }
