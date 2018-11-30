c4d9fee1a32b0cc71f22160035218e4324530d85
Chido Nguyen <chidonguyen@hotmail.com>
2018-11-29 18:22:14 -0800

    Updated partB Final V1.0

--- projects/nguychid/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/nguychid/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -21,7 +21,27 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
+//You can use this function to implement your manual testing	
+	   UrlValidator urlValidator = new UrlValidator(null,null, UrlValidator.ALLOW_ALL_SCHEMES);        
+       String[] urls = {
+               "http://www.ebay.com/",
+               "http://www.google.com/",
+               "http://www.google2.com/",
+               "http://www.google.com/",
+               "http://www.google.com/",
+               "http:google.com/",
+               "http/www.google.com/",
+               "http/www.---.com/",
+               "http://what-everything-go-off-the-rails",
+               "not-a-url"};
+
+       for (String u : urls) {
+           if (urlValidator.isValid(u)) {
+               System.out.println(u + " is valid");
+           } else {
+               System.out.println(u + " is invalid");
+           }
+       }
 	   
    }
    
@@ -40,6 +60,7 @@ public class UrlValidatorTest extends TestCase {
 	   assertFalse(urlVal.isValid(testUrlScheme[6].item+"www.google.com"));
 	   assertFalse(urlVal.isValid(testUrlScheme[7].item+"www.google.com"));*/
 	   
+	
 	   
    }
    
@@ -57,17 +78,59 @@ public class UrlValidatorTest extends TestCase {
 	   assertTrue(urlVal.isValid("http://"+testUrlAuthority[6].item));
 	   //this is right//
 	  //assertFalse(urlVal.isValid("http://"+testUrlAuthority[3].item));
-	 assertFalse(urlVal.isValid("http://"+testUrlAuthority[4]));
+	 //assertFalse(urlVal.isValid("http://"+testUrlAuthority[4].item));
 	 //  assertFalse(urlVal.isValid("http://"+testUrlAuthority[5]));
 	  // assertFalse(urlVal.isValid("http://"+testUrlAuthority[6]));
+	   
+	   /*   for(ResultPair duo: testUrlScheme ) {
+		   
+		   if(duo.valid) {
+			   assertTrue(urlVal.isValid(duo.item + "www.google.com"));
+		   }
+		   else
+			   assertFalse(urlVal.isValid(duo.item + "www.google.com"));
+		   
+	   }*/
 	   
    }
    //You need to create more test cases for your Partitions if you need to 
    
    public void testIsValid()
    {
-	   //You can use this function for programming based testing
+	 //You can use this function for programming based testing
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 
+	   System.out.println("testIsValid() Tests");
+	   
+       // Iterate through every test URL combination
+       for(int a = 0; a < testUrlScheme.length; a++) {
+    	 for(int b = 0; b < testUrlAuthority.length; b++) {
+	       for(int c = 0; c < testUrlPort.length; c++) {
+	    	  for(int d = 0; d < testPath.length; d++) {
+		       for(int e = 0; e < testUrlQuery.length; e++) {
+		    	   boolean expectedResult = testUrlScheme[a].valid && testUrlAuthority[b].valid && testUrlPort[c].valid && testPath[d].valid && testUrlQuery[e].valid;
+		    	   System.out.println("" + testUrlScheme[a].valid + " " + testUrlAuthority[b].valid + " " + testUrlPort[c].valid + " " + testPath[d].valid + " " + testUrlQuery[e].valid);
+
+			   System.out.println("Expected Result Bool: " + expectedResult);
+			   
+			   String url = testUrlScheme[a].item + testUrlAuthority[b].item + testUrlPort[c].item + testPath[d].item + testUrlQuery[e].item;
+
+			   if (expectedResult == true) {
+			       assertTrue(urlVal.isValid(url));
+				   System.out.println("Expected Result: true  Actual Result: " + Boolean.toString(urlVal.isValid(url)));
+				   System.out.println("URL: " + url);
+			   }
+			   else {
+				   // isValid appears to return inconsistent results with bad URLs
+			       assertFalse(urlVal.isValid(url));
+				   System.out.println("Expected Result: false  Actual Result: " + Boolean.toString(urlVal.isValid(url)));
+				   System.out.println("URL: " + url);
+			   }
+		       }
+	       }
+    	 }
+       }
+       }
    }
    
    // Taken from part A //
@@ -90,5 +153,50 @@ public class UrlValidatorTest extends TestCase {
            new ResultPair("www-wrong-multi-format", false),
            new ResultPair("256.256.256.256", false),
            new ResultPair("0.0.0.0.0", false)};
+   // Adding from here
+   ResultPair[] testUrlPort = {new ResultPair(":80", true),
+                              new ResultPair(":65535", true),
+                              new ResultPair(":0", true),
+                              new ResultPair("", true),
+                              new ResultPair(":-1", false),
+                             new ResultPair(":65636",false),
+                              new ResultPair(":65a", false)
+    };
+    ResultPair[] testPath = {new ResultPair("/test1", true),
+                           new ResultPair("/t123", true),
+                           new ResultPair("/$23", true),
+                           new ResultPair("/..", false),
+                           new ResultPair("/../", false),
+                           new ResultPair("/test1/", true),
+                           new ResultPair("", true),
+                           new ResultPair("/test1/file", true),
+                           new ResultPair("/..//file", false),
+                           new ResultPair("/test1//file", false)
+    };
+    //Test allow2slash, noFragment
+    ResultPair[] testUrlPathOptions = {new ResultPair("/test1", true),
+                                     new ResultPair("/t123", true),
+                                     new ResultPair("/$23", true),
+                                     new ResultPair("/..", false),
+                                     new ResultPair("/../", false),
+                                     new ResultPair("/test1/", true),
+                                     new ResultPair("/#", false),
+                                     new ResultPair("", true),
+                                     new ResultPair("/test1/file", true),
+                                     new ResultPair("/t123/file", true),
+                                     new ResultPair("/$23/file", true),
+                                     new ResultPair("/../file", false),
+                                     new ResultPair("/..//file", false),
+                                     new ResultPair("/test1//file", true),
+                                     new ResultPair("/#/file", false)
+    };
+
+    ResultPair[] testUrlQuery = {new ResultPair("?action=view", true),
+                               new ResultPair("?action=edit&mode=up", true),
+                               new ResultPair("", true)
+    };
+
+    Object[] testUrlParts = {testUrlScheme, testUrlAuthority, testUrlPort, testPath, testUrlQuery};
+    //Object[] testUrlPartsOptions = {testUrlScheme, testUrlAuthority, testUrlPort, testUrlPathOptions, testUrlQuery};
           
 }
